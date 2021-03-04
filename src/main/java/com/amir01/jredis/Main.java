60e76d3de0abe8752be662ceec31f941b1934dd8
Amir01 <iran.volleyball79@gmail.com@users.noreply.github.com>
2021-03-05 03:04:23 +0330

    test: Test jedis

--- src/main/java/com/amir01/jredis/Main.java
+++ src/main/java/com/amir01/jredis/Main.java
@@ -1,4 +1,17 @@
 package com.amir01.jredis;
 
+import redis.clients.jedis.Jedis;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Jedis jedis = new Jedis("localhost");
+        jedis.set("foo", "bar");
+        String value = jedis.get("foo");
+        System.out.println("value: " + value);
+        jedis.close();
+
+    }
+
 }
