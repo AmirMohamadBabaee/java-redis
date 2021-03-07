2aef9d337ed20787112a45e0b6717fde41b618c8
Amir01 <iran.volleyball79@gmail.com@users.noreply.github.com>
2021-03-07 17:55:47 +0330

    feat: Complete splitter & parser methods and Handle loop in main

--- src/main/java/com/amir01/jredis/Main.java
+++ src/main/java/com/amir01/jredis/Main.java
@@ -1,17 +1,84 @@
 package com.amir01.jredis;
 
-import redis.clients.jedis.Jedis;
+import java.util.Scanner;
 
 public class Main {
 
+    private static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-        Jedis jedis = new Jedis("localhost");
-        jedis.set("foo", "bar");
-        String value = jedis.get("foo");
-        System.out.println("value: " + value);
-        jedis.close();
+        RedisClient redisClient = RedisClient.getInstance();
+        while(scan.hasNext()) {
+            String command = scan.nextLine();
+            String[] split_command = splitter(command);
+            boolean status = parser(redisClient, split_command);
+            if(status) {
+                break;
+            }
+        }
+        redisClient.jedisClose();
 
+    }
+
+    public static String[] splitter(String command) {
+        return command.split(" ");
+    }
+
+    public static boolean parser(RedisClient redisClient, String[] command_array) {
+
+        if(command_array[0].equals("create")) {
+
+            if(command_array.length == 3) {
+                boolean output = redisClient.create(command_array[1], command_array[2]);
+                System.out.println("[log] " + command_array[0] + " " + command_array[1] + " "
+                    + command_array[2]);
+                System.out.println(output);
+                System.out.println();
+                return false;
+            }
+
+        } else if(command_array[0].equals("fetch")) {
+
+            if(command_array.length == 2) {
+                String output = redisClient.fetch(command_array[1]);
+                boolean isExist = redisClient.isExist(command_array[1]);
+                System.out.println("[log] " + command_array[0] + " " + command_array[1]);
+                System.out.println(isExist);
+                if(isExist) {
+                    System.out.println(output);
+                }
+                System.out.println();
+                return false;
+            }
+
+        } else if(command_array[0].equals("update")) {
+
+            if(command_array.length == 3) {
+                boolean output = redisClient.update(command_array[1], command_array[2]);
+                System.out.println("[log] " + command_array[0] + " " + command_array[1] + " "
+                    + command_array[2]);
+                System.out.println(output);
+                System.out.println();
+                return false;
+            }
+
+        } else if(command_array[0].equals("delete")) {
+
+            if(command_array.length == 2) {
+                boolean output = redisClient.delete(command_array[1]);
+                System.out.println("[log] " + command_array[0] + " " + command_array[1]);
+                System.out.println(output);
+                System.out.println();
+                return false;
+            }
+
+        } else if(command_array[0].equals("exit")) {
+            System.out.println("[log] Stop JRedis...");
+            return true;
+        }
+
+        return false;
     }
 
 }
